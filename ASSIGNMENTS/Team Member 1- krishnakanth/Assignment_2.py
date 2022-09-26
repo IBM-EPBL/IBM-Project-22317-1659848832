507d3b3b91867767d3f400cc167e77d546faff5a
krishnakanth03 <101858199+krishnakanth03@users.noreply.github.com>
2022-09-26 14:47:37 +0530

    Add files via upload

---
+++ ASSIGNMENTS/Team Member 1- krishnakanth/Assignment_2.py
@@ -0,0 +1,11 @@
+import random
+import time
+while(1):
+    temp=random.uniform(10.0,40.0)
+    humid=random.randint(30,90)
+    print("\nTemperature : {:.2f}*C Humidity : {}%" .format(temp,humid))
+    if temp>30:
+        print("\n Warning: Temperature is high")
+    else:
+        print("\n Normal Temperature")
+    time.sleep(1)
